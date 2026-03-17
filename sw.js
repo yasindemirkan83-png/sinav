a1bf09f52bea4c5066642ce1cc4a356a12d859bd
yasindemirkan83-png <yasindemirkan83@gmail.com>
2026-03-17 21:38:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,17 +5,16 @@ const ASSETS = [
   './style.css',
   './app.js',
   './manifest.json',
+  './Logo.png',
   './anfa.gif'
 ];
 
-// Yükleme ve Önbelleğe Alma
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Strateji: Önce Önbellek, Yoksa Ağ
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
